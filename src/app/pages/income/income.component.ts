cf401f9e1c3aeaabd16dd2b0548c871b0a9e6570
pongthep promsiri <pongthep@baac.or.th>
2019-11-16 13:50:07 +0700

    [Add]: call api service

--- src/app/pages/income/income.component.ts
+++ src/app/pages/income/income.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, TemplateRef } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal/public_api';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { Income } from 'src/app/models/income';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-income',
@@ -10,21 +11,18 @@ import { Income } from 'src/app/models/income';
 })
 export class IncomeComponent implements OnInit {
 
-  constructor(private modalService: BsModalService) { }
+  constructor(private modalService: BsModalService,private http: HttpClient) { }
   modalRef: BsModalRef
-  income: Income;
+  incomes: Income[];
 
   ngOnInit() {
-    this.income = {
-      id: 1,
-      incomeGroupId: 1,
-      incomeNameGroupId: 1,
-      amount: 10000,
-      date: "1/3/2019"
-    }
+    this.http.get('http://103.74.254.157:9003/income/id/1')
+             .subscribe((incomes: Income[]) =>{
+               this.incomes = incomes;
+             });
   }
 
-  openModal(template: TemplateRef<any>){
+  openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
   }
 }
