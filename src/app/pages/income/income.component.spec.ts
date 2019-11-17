5be52d992591a1f743f3805aa0458774ea9df25c
pongthep promsiri <pongthep@baac.or.th>
2019-11-17 09:39:27 +0700

    [add] unit test income component

--- src/app/pages/income/income.component.spec.ts
+++ src/app/pages/income/income.component.spec.ts
@@ -1,9 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { IncomeComponent } from './income.component';
 import { ModalModule } from 'ngx-bootstrap/modal';
-import { HttpClientModule } from '@angular/common/http';
 import { IncomeService } from 'src/app/services/income/income.service';
+import { Income } from 'src/app/models/income';
+import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('IncomeComponent', () => {
   let component: IncomeComponent;
@@ -12,25 +14,66 @@ describe('IncomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ IncomeComponent ],
-      imports: [ModalModule.forRoot(), HttpClientModule]
+      declarations: [IncomeComponent],
+      imports: [ModalModule.forRoot(),
+        HttpClientTestingModule,
+        ReactiveFormsModule
+      ]
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(IncomeComponent);
     component = fixture.componentInstance;
-    incomeService = TestBed.inject(IncomeService)
     fixture.detectChanges();
+    incomeService = TestBed.inject(IncomeService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should call getIncomeByUserId service when call ngOnInit',() =>{
-    spyOn(incomeService, 'getIncomeByUserId');
-    component.ngOnInit();
-    expect(incomeService.getIncomeByUserId).toHaveBeenCalled();
+
+  describe('getIncomeByUserId service', () => {
+    let expected: Income[];
+    beforeEach(() => {
+      expected = [{
+        id: 1,
+        incomeGroupId: 1,
+        incomeNameGroupId: 'งานประจำ',
+        amount: 100000,
+        date: '1/31/2019'
+      }
+      ];
+      spyOn(incomeService, 'getIncomeByUserId').and.returnValue(of(expected));
+    });
+
+    it('should call getIncomeByUserId service when call ngOnInit', () => {
+      component.ngOnInit();
+      expect(incomeService.getIncomeByUserId).toHaveBeenCalled();
+    });
+    it('should set incomes when call getIdcomeByUserId is success', () => {
+      component.ngOnInit();
+      expect(component.incomes).toBe(expected);
+
+    });
+  })
+
+
+  describe('create reactive form', () => {
+    it('should set empty in date of form', () => {
+      component.ngOnInit();
+      expect(component.incomeForm.controls.date.value).toBe('');
+    });
+
+    it('should set empty in income group id of form', () => {
+      component.ngOnInit();
+      expect(component.incomeForm.controls.incomeGroupId.value).toBe('');
+    });
+    it('should set empty in amount of form', () => {
+      component.ngOnInit();
+      expect(component.incomeForm.controls.amount.value).toBe('');
+    });
   });
 });
+
--- src/app/pages/income/income.component.ts
+++ src/app/pages/income/income.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
-import { BsModalRef } from 'ngx-bootstrap/modal/public_api';
-import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalService,BsModalRef } from 'ngx-bootstrap/modal';
 import { Income } from 'src/app/models/income';
 import { IncomeService } from 'src/app/services/income/income.service';
+import { FormGroup,FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-income',
@@ -11,16 +11,35 @@ import { IncomeService } from 'src/app/services/income/income.service';
 })
 export class IncomeComponent implements OnInit {
 
-  constructor(private modalService: BsModalService,private incomeService: IncomeService) { }
+  constructor(
+    private modalService: BsModalService,
+    private incomeService: IncomeService,
+    private fb: FormBuilder) { }
   modalRef: BsModalRef
   incomes: Income[];
+  incomeForm: FormGroup;
 
   ngOnInit() {
-  this.incomeService.getIncomeByUserId();
+    this.getIncomeByUserId(); 
+    this.createForm();
   }
+
+  createForm(){
+    this.incomeForm = this.fb.group({
+      date: '',
+      incomeGroupId: '',
+      amount: ''
+    });
+  }
+
+  getIncomeByUserId() {
+    this.incomeService.getIncomeByUserId().subscribe((incomes: Income[]) => {
+      this.incomes = incomes;
+    });
+  }
+
 
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
   }
 }
- 
